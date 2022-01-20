a7a8238cc1a77935bbb562c0338b3b37652a511d
Sverine <severine.sylvan@gmail.com>
2022-01-20 16:12:36 +0100

    add eggshot page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 
-import FaventinePage from "./pages/FaventinePage";
-import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import FaventinePage from "./pages/FaventinePage";
 import ZerosixPage from './pages/ZerosixPage';
 import EtapesPrintPage from './pages/EtapesPrintPage';
 import CourrierPlusPage from './pages/CourrierPlusPage';
+import EggshotPage from './pages/EggshotPage';
 import FiaacPage from './pages/FiaacPage';
 import LaReservePage from './pages/LaReservePage';
 import LeBoncoinPage from './pages/LeBoncoinPage';
@@ -22,9 +23,10 @@ const App =()=> {
         <Route path="/la-reserve-de-la-chapelle" element={<LaReservePage/>}/>
         <Route path="/etapes-print" element={<EtapesPrintPage/>}/>
         <Route path="/courrier-plus" element={<CourrierPlusPage/>}/>
-        <Route path="/muchos-tacos" element={<MuchosTacosPage/>}/>
+        <Route path="/eggshot" element={<EggshotPage/>}/>
         <Route path="/le-boncoin" element={<LeBoncoinPage/>}/>
         <Route path="/fiaac" element={<FiaacPage/>}/>
+        <Route path="/muchos-tacos" element={<MuchosTacosPage/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
