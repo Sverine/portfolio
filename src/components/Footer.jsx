703e6db7719e7639876ea8957e240b15006099f9
Sverine <severine.sylvan@gmail.com>
2022-02-08 22:55:11 +0100

    add socials icons into homepage footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Logo from './Logo';
+import Github from './Svg-components/Github';
+import Linkedin from './Svg-components/Linkedin';
+import Twitter from './Svg-components/Twitter';
 
 const Footer = ({color, content}) => {
 
@@ -8,6 +11,17 @@ const Footer = ({color, content}) => {
             <h3 style={{color:color, fontWeight:"300",fontSize:"14px"}}>{content.thank}</h3>
             <div style={{maxWidth:"235px", margin:"0 auto"}}>
                 <Logo color1={color} color2="#6862CA"/>
+                <div className="socials" style={{marginTop:"20px", display:"flex",justifyContent:'space-evenly'}}>
+                    <a href="https://github.com/Sverine" target="_blank" rel="noreferrer"> 
+                        <Github color={color}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/severine-sylvan/" target="_blank" rel="noreferrer"> 
+                        <Linkedin color={color}/>
+                    </a>
+                    <a href="https://twitter.com/sverinee" target="_blank" rel="noreferrer"> 
+                        <Twitter color={color}/>
+                    </a>
+                </div>
             </div>
         </div>
     );
